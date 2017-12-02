9c1df0b516ee492d8882b25829baaf814d4e820b
Christopher Diep <diep.christopher@gmail.com>
2017-12-02 18:48:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  #one_less = integer - 1
+  count_to_integer_array = (2..Math.sqrt(integer).ceil).to_a
+  count_to_integer_array.each do |num|
+    if integer % num == 0
+      return false
+    else
+      return true
+    end
+  end
+end
